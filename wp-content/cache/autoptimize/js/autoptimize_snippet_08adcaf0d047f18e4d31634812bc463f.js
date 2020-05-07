feeac09b2c30a51c29ddbc29e7eaa84f86c88395
djebel-amila <jmerc001@gold.ac.uk>
2020-05-07 19:29:06 +0200

    WP2Static CREATE wp-content/cache/autoptimize/js/autoptimize_snippet_08adcaf0d047f18e4d31634812bc463f.js

---
+++ wp-content/cache/autoptimize/js/autoptimize_snippet_08adcaf0d047f18e4d31634812bc463f.js
@@ -0,0 +1,6 @@
+try{var swiper_slider;(function($){"use strict";$('[data-slider]').each(function(){var t=$(this),config={},general=t.data('general'),autoplay=t.data('autoplay'),pagination=t.data('pagination'),navigation=t.data('navigation'),breakpoints=t.data('breakpoints');if(general){config=general;}
+if(autoplay){config.autoplay=autoplay;}
+if(pagination){config.pagination=pagination;}
+if(navigation){config.navigation=navigation;}
+if(breakpoints){config.breakpoints=breakpoints;}
+swiper_slider=new Swiper(t,config);t.removeAttr('data-general');t.removeAttr('data-autoplay');t.removeAttr('data-pagination');t.removeAttr('data-navigation');t.removeAttr('data-breakpoints');});})(jQuery);;}catch(e){}
